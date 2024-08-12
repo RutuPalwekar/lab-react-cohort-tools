bf6ee03ebbab1281b516280cbd16124a454f8c23
Rutushree Palwekar <palwekar.rutushree@gmail.com>
2024-08-12 23:34:02 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,34 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import { Routes , Route , Link } from "react-router-dom";
 
 
 function App() {
 
   return (
     <div className="App relative z-20 pt-20">
-      <Navbar />
+      <nav>
+        <Link to="/" className="navbar-logo">
+          <img src="src/assets/logo-ironhack-blue.png" alt="Logo" className="logo" />
+        </Link>
+
+        <Link to="/profile" className="navbar-user-icon">
+          <img
+            src="src/assets/profile-icon.png"
+            alt="User Icon"
+            className="user-icon"
+          />
+        </Link>
+      </nav>
 
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+        </Routes>
       </div>
-
     </div>
   );
 }
--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,5 +1,6 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
+import { Link } from "react-router-dom";
 
 function StudentDetailsPage() {
 
@@ -65,12 +66,10 @@ function StudentDetailsPage() {
               </p>
             </div>
 
-
             {/* Back button */}
-            <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
+            <Link to="/" className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
               Back
-            </button>
-
+            </Link>
           </>
         )}
       </div>
